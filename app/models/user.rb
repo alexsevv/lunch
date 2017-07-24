37b0fca1791cc423778ddbca4ab4732673425c73
alexsevv <aaa123qqq@mail.ru>
2017-07-24 21:15:32 +0300

    add device and twitter-bootstrap-rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :orders
   has_many :soups
   has_many :cereals
   has_many :drinks
-
-  validates :user, presence: true
 
   before_validation :set_name, on: :create
 
